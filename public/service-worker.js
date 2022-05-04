68d35b32511347ef84eb2912d1aae8b339db52c8
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-05-04 03:52:28 -0500

    added icons to cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,14 +6,52 @@ const FILES_TO_CACHE = [
     './index.html',
     './css/styles.css',
     './js/idb.js',
-    './js/index.js'
+    './js/index.js',
+    './icons/icon-72x72.png',
+    './icons/icon-96x96.png',
+    './icons/icon-128x128.png',
+    './icons/icon-144x144.png',
+    './icons/icon-152x152.png',
+    './icons/icon-192x192.png',
+    './icons/icon-384x384.png',
+    './icons/icon-512x512.png'
 ];
 
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-          console.log('installing cache : ' + CACHE_NAME)
-          return cache.addAll(FILES_TO_CACHE)
+// The following 3 event listener code blocks were copied and modified from the University of Texas Code Bootcamp Module 19 course project.
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE)
+    })
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys()
+    .then(function(keylist) {
+      let cacheKeepList = keylist.filter(function(key) {
+        return key.indexOf(PREFIX);
+      });
+
+      cacheKeepList.push(CACHE_NAME)
+
+      return Promise.all(
+        keylist.map(function(key, i) {
+          if (cacheKeepList.indexOf(key) === -1) {
+            return caches.delete(keylist[i]);
+          }
         })
       )
+    })
+  )
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(request) {
+      return request || fetch(event.request)
+    })
+  )
 });
